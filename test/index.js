00ac56837e334ab485f9e1e2317ca3c2297b7274
Josh Taylor <joshrtay@gmail.com>
2015-12-13 23:49:35 -0800

    improve code style

--- test/index.js
+++ test/index.js
@@ -20,7 +20,7 @@ test('should work with arrays, objects and generators', function (t) {
   t.deepEqual(map(add1, [1, 2, 3]), [2, 3, 4])
   t.deepEqual(map(add1, {a: 1, b: 2, c: 3}), {a: 2, b: 3, c: 4})
   if (supportsGen()) {
-    eval("t.deepEqual(Array.from(map(add1, function * () {yield 1; yield 2; yield 3})()), [2, 3, 4])")
+    eval('t.deepEqual(Array.from(map(add1, function * () {yield 1; yield 2; yield 3})()), [2, 3, 4])')
   }
 })
 
@@ -28,18 +28,16 @@ test('should work with functor', function (t) {
   var f = new Functor([1, 2, 3])
   t.deepEqual(map(add1, f), [2, 3, 4])
   t.end()
-
-
 })
 
 function add1 (n) {
   return n + 1
 }
 
-function Functor(data) {
+function Functor (data) {
   this.data = data
 }
 
-Functor.prototype.map = function(fn) {
+Functor.prototype.map = function (fn) {
   return this.data.map(fn)
 }
