701079308053c1e50dbe31837dbfd5f832d992e5
Josh Taylor <joshrtay@gmail.com>
2015-12-28 12:41:27 -0800

    add iterator support

--- test/index.js
+++ test/index.js
@@ -10,18 +10,12 @@ var supportsGen = require('@f/supports-gen')
  * Tests
  */
 
-test('should work with arrays, objects and generators', function (t) {
-  if (supportsGen()) {
-    t.plan(3)
-  } else {
-    t.plan(2)
-  }
-
+test('should work with arrays, objects, generators, and iterators', function (t) {
   t.deepEqual(map(add1, [1, 2, 3]), [2, 3, 4])
   t.deepEqual(map(add1, {a: 1, b: 2, c: 3}), {a: 2, b: 3, c: 4})
-  if (supportsGen()) {
-    eval('t.deepEqual(Array.from(map(add1, function * () {yield 1; yield 2; yield 3})()), [2, 3, 4])')
-  }
+  t.deepEqual(Array.from(map(add1, function * () {yield 1; yield 2; yield 3})()), [2, 3, 4])
+  t.deepEqual(Array.from(map(add1, function * () {yield 1; yield 2; yield 3} ())()), [2, 3, 4])
+  t.end()
 })
 
 test('should work with functor', function (t) {
